01a5524576a2aa1001fa9fb98d268e1ce377813e
Piotr Szulc <ps347277@students.mimuw.edu.pl>
2019-06-10 13:28:15 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-ghc -o lakke generated/*.hs src/**/*.hs src/**/**/*.hs Main.hs
+# add bnfc to path
+PATH=/home/students/inf/PUBLIC/MRJP/bin/:$PATH
+
+#remove generated
+rm -rf generated
+
+#generate grammar
+make generate_grammar
+
+#rm test
+rm generated/TestLakke.hs
+
+ghc -o lakke generated/*.hs src/Typechecker/*.hs src/Interpreter/*.hs src/Interpreter/Semantics/*.hs  Main.hs
